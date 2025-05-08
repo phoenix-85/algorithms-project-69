7f735bf5a6bb6878c8be66cd29572c55a7ad9193
phoenix <phoenix-85@github.com>
2025-05-08 21:02:50 +0500

    step 6

--- src/index.php
+++ src/index.php
@@ -5,7 +5,17 @@ namespace App;
 function search(array $docs, string $search): array
 {
     $result = [];
-    $invertedIndex = [];
+
+    $allText = implode(' ', array_column($docs, 'text'));
+    preg_match_all('/\w+/', $allText, $matches);
+    $allWords = array_unique($matches[0]);
+    $allDocs = array_column($docs, 'id');
+    $countDocs = count($docs);
+
+    $invertedIndex = array_fill_keys($allWords, []);
+    $tf = array_fill_keys($allDocs, []);
+    $idf = [];
+    $tfidf = [];
 
     foreach ($docs as $doc) {
         ['id' => $id, 'text' => $text] = $doc;
@@ -14,24 +24,39 @@ function search(array $docs, string $search): array
         preg_match_all('/\w+/', $text, $textWords);
 
         $rankArray = [];
-        $wordCount = 0;
+        $tfWords = array_fill_keys(array_unique($textWords[0]), 0);
 
         foreach ($textWords[0] as $textWord) {
-            $invertedIndex[$textWord][] = $id;
+            (in_array($id, $invertedIndex[$textWord])) ? : $invertedIndex[$textWord][] = $id;
+            $tfWords[$textWord]++;
         }
 
-        $invertedIndex = array_map(fn($item) => array_unique($item), $invertedIndex);
+        $textWordsCount = count($textWords[0]);
+
+        foreach ($tfWords as $word => $count) {
+            $tf[$id][$word] = $count / $textWordsCount;
+        }
 
         foreach ($searchWords[0] as $searchWord) {
             $count = count(array_filter($textWords[0], fn($word) => $word == $searchWord));
             $rankArray[] = $count;
-            $wordCount += $count;
         }
 
         $rank = min($rankArray);
+        $wordCount = array_sum($rankArray);
 
         if (($rank > 0) || ($wordCount > 0)) {
             $result[$id] = $rank;
+        }
+    }
+
+    foreach ($invertedIndex as $word => $docs) {
+        $idf[$word] = log10($countDocs / count($docs));
+    }
+
+    foreach ($tf as $doc => $words) {
+        foreach ($words as $word => $index) {
+            $tfidf[$doc][$word] = $index * $idf[$word];
         }
     }
 
